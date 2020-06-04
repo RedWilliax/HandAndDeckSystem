82533e690ad757d06f407570fd0acf65200755af
William Morelli <morelli186@gmail.com>
2020-06-04 10:07:05 +0200

    Begin

---
+++ HandAndDeckSystem/Assets/Scripts/HAD_Container.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HAD_Container : MonoBehaviour
+{
+
+    List<HAD_Card> cards = new List<HAD_Card>();
+
+    [SerializeField] int maxCards = 10;
+
+    #region Handler
+
+    public void AddCard(HAD_Card _card)
+    {
+        cards.Add(_card);
+    }
+
+    public void RemoveCard(HAD_Card _card)
+    {
+        cards.Remove(_card);
+    }
+
+    #endregion
+
+
+
+
+}
