780ec9b97363f78b80bb3f8281e4c9246c7a2414
William Morelli <morelli186@gmail.com>
2021-12-14 23:09:00 +0100

    CardCreatorTool (WIP)

--- HandAndDeckSystem/Assets/Scripts/Cards/HAD_Card.cs
+++ HandAndDeckSystem/Assets/Scripts/Cards/HAD_Card.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public enum ECardStat
 {
     NONE,
     Atck,
     Def,
+    Life,
     Cost,
     Exp
 
 }
 
+[Serializable]
 public struct StatCard
 {
     ECardStat stat;
@@ -23,16 +26,61 @@ public struct StatCard
     public ECardStat Stat { get => stat; set => stat = value; }
 }
 
+[Serializable]
+public struct DataCard
+{
+    string name;
+
+    string nameSprite;
+
+    ERarity rarity;
+
+    List<StatCard> statCards;
+
+    public string Name { get => name; set => name = value; }
+    public string NameSprite { get => nameSprite; set => nameSprite = value; }
+    public ERarity Rarity { get => rarity; set => rarity = value; }
+    public List<StatCard> StatCards { get => statCards; set => statCards = value; }
+
+    #region ManageStat
+
+    public void AddStat(StatCard _stat)
+    {
+        ManageStatCard(true, _stat);
+    }
+
+    public void RemoveStat(StatCard _stat)
+    {
+        ManageStatCard(false, _stat);
+    }
+
+    private void ManageStatCard(bool _add, StatCard _stat)
+    {
+        if (_add ? ExistStat(_stat) : !ExistStat(_stat)) return;
+
+        if (_add)
+            statCards.Add(_stat);
+        else
+            statCards.Remove(_stat);
+    }
+
+    private bool ExistStat(StatCard stat)
+    {
+        return statCards.Any(n => n.Stat == stat.Stat) || stat.Stat == ECardStat.NONE;
+    }
+
+    #endregion
+
+}
+
 //Rename class
 public class HAD_Card : MonoBehaviour
 {
-    List<StatCard> allStats = new List<StatCard>();
+    DataCard dataCard;
 
-    public string Name { get; set; } = "Default_Name";
-    public ERarity Rarity { get; set; } = ERarity.COMMON;
     public HAD_Player Owner { get; set; }
     public Vector3 Anchor { get; set; }
- 
+
     public virtual void ActionCard() { }
 
     public void SetPositon(Vector3 _newPos)
--- HandAndDeckSystem/Assets/Scripts/Tools/CardCreator/Editor/HAD_CardCreatorWindow.cs
+++ HandAndDeckSystem/Assets/Scripts/Tools/CardCreator/Editor/HAD_CardCreatorWindow.cs
@@ -5,7 +5,11 @@ using UnityEditor;
 
 public class HAD_CardCreatorWindow : EditorWindow
 {
-    HAD_Card cardtoCreate = null;
+    static DataCard dataCard;
+    static StatCard newStat;
+
+    Vector2 scrollStats;
+
 
     [MenuItem("Tools/Card/CardCreator")]
     static void Init()
@@ -14,27 +18,92 @@ public class HAD_CardCreatorWindow : EditorWindow
 
         window.Show();
 
+        dataCard = new DataCard();
+
+        dataCard.StatCards = new List<StatCard>();
+
+        newStat = new StatCard();
+
     }
+
     private void OnGUI()
     {
-        
+
+        SetName();
 
         EditorGUILayout.Space();
 
+        SetRarity();
+
+        EditorGUILayout.Space();
+
+        scrollStats = EditorGUILayout.BeginScrollView(scrollStats);
+
+        StatsManager();
+
+        EditorGUILayout.Space();
+
+        AddStat();
+
+        EditorGUILayout.EndScrollView();
+
+        EditorGUILayout.Space();
 
         if (GUILayout.Button("Create Card"))
         {
-            CreateCard(cardtoCreate);
+            //CreateCard(cardtoCreate);
         }
     }
 
     #region CardCreator
 
-    void AddStat(StatCard _stat)
+    void SetName()
     {
-        
+        dataCard.Name = EditorGUILayout.TextField("Card's Name :", dataCard.Name);
     }
 
+    void SetRarity()
+    {
+        dataCard.Rarity = (ERarity)EditorGUILayout.EnumPopup("Rarity's Card : ", dataCard.Rarity);
+    }
+
+
+    void StatsManager()
+    {
+        for (int i = 0; i < dataCard.StatCards.Count; i++)
+        {
+            StatCard _currentStat = dataCard.StatCards[i];
+
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.BeginVertical();
+
+            _currentStat.Stat = (ECardStat)EditorGUILayout.EnumPopup("Type's Stat : ", _currentStat.Stat);
+
+            _currentStat.Data = EditorGUILayout.FloatField("Data's Sata : ", _currentStat.Data);
+
+            EditorGUILayout.EndVertical();
+
+            dataCard.StatCards[i] = _currentStat;
+
+            if (GUILayout.Button(" X "))
+                dataCard.RemoveStat(_currentStat);
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+        }
+    }
+
+    void AddStat()
+    {
+        newStat.Stat = (ECardStat)EditorGUILayout.EnumPopup("Type's Stat : ", newStat.Stat);
+
+        newStat.Data = EditorGUILayout.FloatField("Data's Sata : ", newStat.Data);
+
+        if (GUILayout.Button(" Add Stat "))
+            dataCard.AddStat(newStat);
+    }
 
     #endregion
 
